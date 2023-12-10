ede2f7762b5486d6841ec43639a3929a18efcaa5
baharkose <baharkse17@gmail.com>
2023-12-10 08:26:00 +0300

    added comment line

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -4,31 +4,43 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 const Books = ({ bookList }) => {
-  const [choicenBook, setChoicenBook] = useState("")
+  const [choicenBook, setChoicenBook] = useState("");
 
   return (
     <Container className="d-flex flex-wrap gap-4 mt-5 justify-content-center m-auto ">
       {bookList.map(({ id, title, author, description, image_url }) => (
         <Card
           className="mb-4 d-flex justify-content-center "
-          style={{ width: choicenBook == id  ? "25rem" : "15rem"}}
+          style={{ width: choicenBook == id ? "25rem" : "15rem" }}
           key={id}
           onClick={() => setChoicenBook(id)}
+          onDoubleClick={() => setChoicenBook("")}
+          //+ sadece seçili alanı görüntülemek için id ataması yaptık.
+          //+ burayı bir incele
         >
+          <div className="d-flex">
+
           <Card.Img
             variant="top"
             className="btn"
             src={image_url}
-            style={{ width: choicenBook == id ? "50%" : "15rem", height: "100%" }}
+            style={{
+              width: choicenBook == id ? "50%" : "15rem",
+              height: "100%",
+            }}
           />
-          <Card.Body style={{ display: choicenBook == id  ? "block" : "none",width: choicenBook == id ? "40%": "" }}>
-            
-              <Card.Title>{title}</Card.Title>
-              <Card.Text>{author}</Card.Text>
-              <Card.Text>{description}</Card.Text>
-              <Button variant="primary">Add To List</Button>
-           
+          <Card.Body
+            style={{
+              display: choicenBook == id ? "block" : "none",
+              width: choicenBook == id ? "40%" : "",
+            }}
+          >
+            <Card.Title>{title}</Card.Title>
+            <Card.Text>{author}</Card.Text>
+            <Card.Text>{description}</Card.Text>
+            <Button variant="primary">Add To List</Button>
           </Card.Body>
+          </div>
         </Card>
       ))}
     </Container>
